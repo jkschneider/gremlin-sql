be5c2a95c50d5233a606aac4ccf737d4d1b4bcba
Jon Schneider <jkschneider@gmail.com>
2014-08-13 23:13:39 -0500

    The most basic of select queries is now working!

--- src/main/groovy/com/github/jkschneider/gremlinsql/GremlinSqlMappingVisitor.groovy
+++ src/main/groovy/com/github/jkschneider/gremlinsql/GremlinSqlMappingVisitor.groovy
@@ -2,8 +2,12 @@ package com.github.jkschneider.gremlinsql
 
 import com.github.jkschneider.gremlinsql.grammar.GremlinSqlBaseListener
 import com.github.jkschneider.gremlinsql.grammar.GremlinSqlParser
+import com.google.common.collect.LinkedListMultimap
+import com.google.common.collect.Multimap
+import com.google.common.collect.TreeMultimap
 import com.tinkerpop.blueprints.Graph
 import com.tinkerpop.gremlin.java.GremlinPipeline
+import org.antlr.v4.runtime.misc.MultiMap
 import org.antlr.v4.runtime.misc.NotNull
 
 /**
@@ -17,15 +21,20 @@ class GremlinSqlMappingVisitor extends GremlinSqlBaseListener {
         this.g = g
     }
 
+    Multimap<String, GremlinPipeline> pipesByLabel = LinkedListMultimap.create();
+
     @Override
     void enterSelect(@NotNull GremlinSqlParser.SelectContext ctx) {
-        String tableName = ctx.table_name().any_name().text
-        System.out.println(tableName);
+        String tableName = ctx.table_name().text
+        pipesByLabel.put(tableName, new GremlinPipeline().has('_label', tableName))
         super.enterSelect(ctx)
     }
 
     @Override
     void exitSelect(@NotNull GremlinSqlParser.SelectContext ctx) {
+        pipeline = pipesByLabel.values().inject(g.V) { pipe, nextPipe ->
+            pipe.step(nextPipe)
+        }
         super.exitSelect(ctx)
     }
 }
